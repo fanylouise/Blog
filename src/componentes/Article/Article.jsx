c2f7548b79cc643d69a0ed821cb927311e5cdf61
fanylouise <gandra.fany@gmail.com>
2022-09-15 18:49:42 -0300

    Adding JSON server

--- src/componentes/Article/Article.jsx
+++ src/componentes/Article/Article.jsx
@@ -6,9 +6,21 @@ import Post from '../Post/Post';
 
 const Article = () => {
   return (
-  <>
-   <Post/>
-  </>
+  <section className='container'>
+    <div className='container-posts'>
+      <Post subtitle='tecnologia' title='Steam Deck mais barato'  author='Chupeta De Galinha' datatime='Setembro 13, 2022' textpost='1Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptatem earum dolorem. Est, voluptas quaerat.' imgprofile=''/>
+      
+      
+
+      <Post  subtitle='fotografia' title='Câmeras que prometem Zoom de 90X'  author='Jake Daniels' datatime='Setembro 14, 2022' textpost='2Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptatem earum dolorem. Est, voluptas quaerat.' imgprofile=''/>
+      
+
+      <Post  subtitle='cinema' title='Bilheteria de Thor Amor e Trovão  supera os  US$ 700 milhões ' author='Suvaco Pereira' datatime='Setembro 17, 2022' textpost='3Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptatem earum dolorem. Est, voluptas quaerat.' imgprofile=''/>
+      
+     
+    </div>
+ 
+  </section>
   );
 }
  
--- src/componentes/Post/Post.jsx
+++ src/componentes/Post/Post.jsx
@@ -1,6 +1,22 @@
-const Post = () => {
+import '../Post/Post.css'
+
+const Post = (props) => {
   return ( 
-    <h1>Posts</h1>
+    <div className="post">
+      <div className="img-post"></div>
+      <h5>{props.subtitle}</h5>
+      <h3>{props.title}</h3>
+      <p>
+      {props.textpost}
+      </p>
+      <div className="flex">
+        <div className="img-profile">{props.imgprofile}</div>
+        <div className="description-profile">
+          <h6 className='person-name'>{props.author}</h6>
+          <p>{props.datatime}</p>
+        </div>
+      </div>
+    </div>
   );
 }
  
